d57e854cef524001c4b75d7576adf0def00961b5
ilegra.douglas <douglas.vargas@ilegra.com>
2019-09-30 01:25:28 -0300

    adicionando swagger

--- src/main/java/com/github/dodivargas/assemblageservice/api/rest/v1/RullingApi.java
+++ src/main/java/com/github/dodivargas/assemblageservice/api/rest/v1/RullingApi.java
@@ -5,6 +5,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.dodivargas.assemblageservice.api.rest.v1.request.RulingRequest;
 import com.github.dodivargas.assemblageservice.api.rest.v1.response.RulingResponse;
 import com.github.dodivargas.assemblageservice.service.RulingService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,11 +24,19 @@ public class RullingApi {
     private RulingService rulingService;
     private ObjectMapper objectMapper;
 
+    private static final int OK = 200;
+    private static final String OK_MESSAGE = "Operação realizada com sucesso.";
+    private static final int CREATED = 201;
+    private static final String CREATED_MESSAGE = "Operação foi realizada e resultou em um novo recurso.";
+
+
     public RullingApi(RulingService rulingService, ObjectMapper objectMapper) {
         this.rulingService = rulingService;
         this.objectMapper = objectMapper;
     }
 
+    @ApiOperation(value = "Create a ruling")
+    @ApiResponses(@ApiResponse(code = OK, message = OK_MESSAGE))
     @PostMapping
     public ResponseEntity<?> create(@RequestBody RulingRequest rulingRequest) {
         return ResponseEntity
@@ -33,6 +44,8 @@ public class RullingApi {
     }
 
 
+    @ApiOperation(value = "Open ruling for voting")
+    @ApiResponses(@ApiResponse(code = CREATED, message = CREATED_MESSAGE))
     @PostMapping(value = "{id}/openForVote")
     public ResponseEntity<?> openForVote(@PathVariable("id") Integer rulingId,
                                          @RequestParam(required = false) Integer openedTime) {
